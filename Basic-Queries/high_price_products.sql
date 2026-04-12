09f28e0a404edbd4aa4d081a44a71e8571e57d85
selokaranjali91-lab <selokaranjali91@gmail.com>
2026-04-12 11:14:39 +0530

    Create high_price_products.sql

---
+++ Basic-Queries/high_price_products.sql
@@ -0,0 +1,8 @@
+-- Problem: High Price of Products
+-- Platform: CodeChef SQL Practice
+-- Topic: WHERE clause
+-- Description: Find product_name and category where price > 100.
+
+SELECT product_name, category
+FROM Products
+WHERE price > 100.00;
