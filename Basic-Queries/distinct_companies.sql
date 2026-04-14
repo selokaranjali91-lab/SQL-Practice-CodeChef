960b7e78aac51298e67324dd987f31dce3d1b0e2
selokaranjali91-lab <selokaranjali91@gmail.com>
2026-04-14 23:59:35 +0530

    Create distinct_companies.sql

---
+++ Basic-Queries/distinct_companies.sql
@@ -0,0 +1,7 @@
+-- Problem: Distinct Companies
+-- Platform: CodeChef SQL Practice
+-- Topic: DISTINCT keyword
+-- Description: Retrieve all unique company names from the Works table.
+
+SELECT DISTINCT company_name
+FROM Works;
