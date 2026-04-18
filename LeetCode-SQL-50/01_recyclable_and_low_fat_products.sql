a3b988d9cd080d8d2ab3a98b67144c0dea4f9205
selokaranjali91-lab <selokaranjali91@gmail.com>
2026-04-18 22:56:08 +0530

    Delete LeetCode-SQL-50/01_recyclable_and_low_fat_products.sql

--- LeetCode-SQL-50/01_recyclable_and_low_fat_products.sql
+++
@@ -1,9 +0,0 @@
--- Problem No: 1757
--- Problem Name: Recyclable and Low Fat Products
--- Platform: LeetCode
--- Difficulty: Easy
-
-SELECT product_id
-FROM Products
-WHERE low_fats = 'Y'
-AND recyclable = 'Y';
